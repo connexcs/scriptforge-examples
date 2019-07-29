d6bb84b208626880c594b37b0fe9d1aed954dd8e
Jonathan Hulme <jon@digipigeon.com>
2019-07-29 12:12:19 +0100

    Create did-driver.js

---
+++ did-driver.js
@@ -0,0 +1,17 @@
+module.export = {
+	main: function (vars) {
+
+	},
+	search: async function (vars) {
+
+	},
+	allocate: async function (vars) {
+
+	},
+	release: async function (vars) {
+	
+	},
+	list: async function () {
+		
+	}
+}
