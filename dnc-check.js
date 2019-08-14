48347ad773cbce8ab6293bc6a10b8ef62847bbc2
Jonathan Hulme <jon@digipigeon.com>
2019-08-14 17:29:05 +0100

    Update dnc-check.js

--- dnc-check.js
+++ dnc-check.js
@@ -1,9 +1,9 @@
 const userspace = require('cxUserspace');
 
-function main(data){
-	return userspace.read('dnc', data.routing.dest_number).then(data => {
+function main(vars){
+	return userspace.read('dnc', vars.routing.dest_number).then(data => {
 		if (data) return Promise.reject([604, 'Number on DNC List']);
-			return data
+			return vars
 		}).catch(err => {
 			if (Array.isArray(err)) return Promise.reject(err);
 			return Promise.reject([500, err]);
