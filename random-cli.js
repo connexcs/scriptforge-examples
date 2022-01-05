eb90c75216a3f37e98a1e0d8177148abdc0cf0dd
Jonathan Hulme <jon@digipigeon.com>
2022-01-05 17:41:45 +0000

    Check for invalid gateway details

--- random-cli.js
+++ random-cli.js
@@ -1,12 +1,14 @@
 function main(vars){
-  var cliList = ["1111111","2222222","3333333","4444444"];
-  vars.routing.cli = cliList[Math.floor(Math.random() * cliList.length)];
+	var cliList = ["1111111","2222222","3333333","4444444"];
+	vars.routing.cli = cliList[Math.floor(Math.random() * cliList.length)];
 	if (vars.routing.egress_routing) {
 		vars.routing.egress_routing.forEach(er => {
-			Object.keys(er.gw).forEach(id => {
-				er.gw[id].cli = vars.routing.cli;
-			});
+			if (er.gw) {
+				Object.keys(er.gw).forEach(id => {
+					er.gw[id].cli = vars.routing.cli;
+				});
+			}
 		});
 	}
-  return vars;
+	return vars;
 }
