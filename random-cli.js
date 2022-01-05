de375b0becba561572eba3f6125f65d118bb188b
Jonathan Hulme <jon@digipigeon.com>
2022-01-05 09:28:35 +0000

    Fix changes in egress routing.

--- random-cli.js
+++ random-cli.js
@@ -1,5 +1,12 @@
 function main(vars){
   var cliList = ["1111111","2222222","3333333","4444444"];
   vars.routing.cli = cliList[Math.floor(Math.random() * cliList.length)];
+	if (vars.routing.egress_routing) {
+		vars.routing.egress_routing.forEach(er => {
+			Object.keys(er.gw).forEach(id => {
+				er.gw[id].cli = vars.routing.cli;
+			});
+		});
+	}
   return vars;
 }
